fc5b7148f2417694a0cbb48e9e1229279362cc79
JayMales <s3486715@student.rmit.edu.au>
2018-09-28 03:18:00 +1000

    Changed layout of code, menu is it's own function now and all tasks use that function

--- Led_Konfigurators3486715.sh
+++ Led_Konfigurators3486715.sh
@@ -1,58 +1,88 @@
 #!/bin/bash
 
-leds="cd /sys/class/leds"
-count=0
+LEDS="cd /sys/class/leds"
+COUNT=0
+INPUT=0
+VAILD=true
+CURRENTSELECT=""
 
 counter(){
-	count=$(($count++))
+	((COUNT+=1))
+}
+
+inputTest(){
+	re="^[0-9]*$"
+	if [[ $INPUT =~ $re && $INPUT -le $COUNT ]];then 
+		VAILD=true
+	else 
+		VAILD=false
+	fi
+}
+
+menuPrinter(){
+	TITLES=("${!1}")
+	OPTIONS=("${!2}")
+	END=false
+	while ! $END; do
+		for test in "${TITLES[@]}";do
+			echo $test
+		done
+		
+		COUNT=0
+		for OPTION in "${OPTIONS[@]}"; do
+			counter
+			echo $COUNT")"$OPTION
+		done
+		echo -n "Please enter a number (1-$COUNT):for the option to configure: "
+		read INPUT
+		inputTest
+		if $VAILD;then END=true; else END=false; 
+			echo "Invaild input, Please try again"; fi
+		echo
+	done
 }
 
 task5(){
-	setthis=`$leds/$1/ && echo "heartbeat" >trigger`
-	theTriggers=`$leds/$1/ && cat trigger`
-	count=1
-	echo "Associate Led with a system Event
-=================================
-Available events are: 
----------------------"
-	for trigger in $theTriggers
-	do
-		counter
-		echo $count $trigger
+	while :;do
+		#SETTHIS=`$LEDS/$CURRENTSELECT/ && echo "heartbeat" >trigger`
+		THETRIGGERS=`$LEDS/$CURRENTSELECT/ && cat trigger`
+		TITLE=("Associate Led with a system Event"
+			"================================="
+			"Available events are:" "---------------------")
+		OPTIONS=("${THETRIGGERS[@]}" "Quit")
+		menuPrinter TITLE[@] OPTIONS[@]
+		if [[ "$INPUT" == "$COUNT" ]];then break; fi
 	done
-	echo
 }
 
 task3(){
-	ledName=$1
-	options=("turn on" "turn off" "associate with a system event" 
-	"associate with the performance of a process" 
-	"stop association with a process’ performance" "quit to main menu")
-	count=1
-	echo $ledName
-	echo ==========
-	for option in "${options[@]}";
-	do
-		counter
-		echo $count $option
+	while :;do
+		TITLE=($CURRENTSELECT "==========")
+		OPTIONS=("Turn on" "Turn off" "Associate with a system event" 
+		"Associate with the performance of a process" 
+		"Stop association with a process’ performance" "Quit to main menu")
+		menuPrinter TITLE[@] OPTIONS[@]
+		if [[ "$INPUT" == "$COUNT" ]];then break; fi
+		task5
 	done
-	echo "Please enter a number (1-6) for your choice:"
-	echo
-	task5 $ledName
 }
 
-names=`$leds && ls`
+task1(){
+	reset
+	while :;do
+		NAMES=(`$LEDS && ls`)
+		TITLE=("Welcome to Led_Konfigurator!" 
+		"============================" "Please select an led to configure: ")
+		OPTIONS=("${NAMES[@]}" "Quit")
+		menuPrinter TITLE[@] OPTIONS[@]
+		if [[ "$INPUT" -eq "$COUNT" ]]
+		then
+			echo "Cya Later Boii"
+			exit 0
+		fi
+		CURRENTSELECT=${NAMES[$INPUT-1]}
+		task3
+	done
+}
 
-echo "Welcome to Led_Konfigurator!
-============================
-Please select an led to configure: "
-count=1
-for name in $names
-do
-	counter
-	echo $count $name
-	task3 $name
-done
-counter
-echo $count "Quit"
-echo "Please enter a number (1-$count):for the led to configure or quit:"
+task1
