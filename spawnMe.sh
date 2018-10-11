67c36f3618e623f7ebeb99f5ccb5a59aa0c94016
Jay <30455994+JayMales@users.noreply.github.com>
2018-10-11 17:36:02 +1100

    Fixed small spacing error

--- spawnMe.sh
+++ spawnMe.sh
@@ -14,7 +14,7 @@ checkPS(){
 	# This finds the usage out of 100% then +1 so no div from 0. Then divided by 100 so I get 0-10 input. 
 	# It then -11 from that so 1 becomes -10 and 10 becomes -1. Then finally times by -1 which makes it a pos number again
 	# So if it is under full load, it will flash every 0.1 seconds and under 10% load it will flash every second (on and off) 
-	USAGE=`bc <<< "((($USAGE+1)/100-11)*-1)/10"`
+	USAGE=`bc <<< "((($USAGE+1)/100 -11)* -1)/10"`
 	`$LEDS/$CURRENTSELECT/ && echo "1" >brightness`
 	sleep 0.2
 	`$LEDS/$CURRENTSELECT/ && echo "0" >brightness`
