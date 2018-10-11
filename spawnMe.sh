35c30c516781bb1836ec5c440fcfb6249e2602f7
Jay <30455994+JayMales@users.noreply.github.com>
2018-10-11 17:34:07 +1100

    Commented code and Fixed flash algorithm

--- spawnMe.sh
+++ spawnMe.sh
@@ -5,18 +5,23 @@ CURRENTSELECT="$1"
 PROCESS="$2"
 CPURAM="$3"
 
-
+# Uses ps to find the memory/cpu usage or search for the process name. Sums up all the usage together then prints sends it to an array
+# Then does some maths on the usage to find out how much it should flash. Sleeps based off the usage
 checkPS(){
-	PSUNSORTEDSTRING="`ps -eo comm,%$CPURAM --no-headers --sort comm| grep $PROCESS | awk '{sum+=$2} END {printf "%s %s\n" ,$1,sum}'`"
+	PSUNSORTEDSTRING="`ps -eo comm,%$CPURAM --no-headers --sort comm| grep $PROCESS | awk '{sum+=$NF} END {printf "%s %s\n" ,$1,sum}'`"
 	PSUNSORTED=($PSUNSORTEDSTRING)
 	USAGE=${PSUNSORTED[-1]}
-	USAGE=`bc <<< "($USAGE+1)/50"`
+	# This finds the usage out of 100% then +1 so no div from 0. Then divided by 100 so I get 0-10 input. 
+	# It then -11 from that so 1 becomes -10 and 10 becomes -1. Then finally times by -1 which makes it a pos number again
+	# So if it is under full load, it will flash every 0.1 seconds and under 10% load it will flash every second (on and off) 
+	USAGE=`bc <<< "((($USAGE+1)/100-11)*-1)/10"`
 	`$LEDS/$CURRENTSELECT/ && echo "1" >brightness`
-	sleep $USAGE
+	sleep 0.2
 	`$LEDS/$CURRENTSELECT/ && echo "0" >brightness`
 	sleep $USAGE
 }
 
+# just loops the checkPS function
 keepMeALive(){
 	while :; do
 		checkPS
